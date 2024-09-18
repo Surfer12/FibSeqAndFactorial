f959cbcafa526f0e7fd8c470bb0975a566b1fc84
Surfer12 <ryanoatsie@outlook.com>
2024-09-18 07:54:47 -0700

    Initial commit

---
+++ src/FactorialClasses/Factorial.java
@@ -0,0 +1,26 @@
+
+
+public class Factorial {
+    private int number;
+
+    public Factorial(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+    public int calculateFactorial() {
+        int result = 1; // Initialize initial value to 1
+        for (int i = 1; i <= number; i++) { // loop from 1 to the number
+            result *= i; // Multiply result by i to calculate the factorial. 
+        }
+        return result; // Return the factorial
+    }
+}
---
+++ src/FactorialClasses/FactorialIteratorEx2.java
@@ -0,0 +1,45 @@
+
+import java.util.Iterator;
+
+// next method, hasNext method, remove method
+//impliments iterator interface to calculate the factorial of a given number
+public class FactorialIteratorEx2 implements Iterator<Integer> {
+    private int aNumberForFactorialCalculation;
+    private int aBaseCasePlaceHolder = 1;
+
+    public FactorialIteratorEx2(int aNumberForFactorialCalculation) {
+        this.aNumberForFactorialCalculation = aNumberForFactorialCalculation;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return aNumberForFactorialCalculation >= aBaseCasePlaceHolder;
+    }
+
+    @Override
+    public Integer next() {
+        return aNumberForFactorialCalculation--;
+    }
+
+    public int theFactorialOfTheNumber() {
+        for (int i = 1; i <= aNumberForFactorialCalculation; i++) {
+            aBaseCasePlaceHolder *= i;
+        }
+        return aBaseCasePlaceHolder;
+    }
+
+    public int theFactorialOfTheNumberUsingRecursion(int aNumberForFactorialCalculation) {
+        if (aNumberForFactorialCalculation == 0) {
+            return 1;
+        }
+        return aNumberForFactorialCalculation * theFactorialOfTheNumberUsingRecursion(aNumberForFactorialCalculation - 1);
+    }
+
+    public int theFactorialOfTheNumberUsingIterator() {
+        Iterator<Integer> iterator = new FactorialIteratorEx2(aNumberForFactorialCalculation);
+        while (iterator.hasNext()) {
+            aBaseCasePlaceHolder *= iterator.next();
+        }
+        return aBaseCasePlaceHolder;
+    }
+}
