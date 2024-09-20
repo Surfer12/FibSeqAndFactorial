1000c02f09099f52b7dfbee835e4abaabcf3a423
Surfer12 <ryanoatsie@outlook.com>
2024-09-19 21:16:26 -0700

    Refactor InsertionAndSelectionSortTest class and add additional test cases

--- src/test/InsertionAndSelectionSortTest.java
+++ src/test/InsertionAndSelectionSortTest.java
@@ -1,76 +1,106 @@
 package test;
 
-import org.junit.jupiter.api.Test;
-
 import Assignment.SortAlgo.InsertionAndSelectionSort;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InsertionAndSelectionSortTest {
 
    @Test
-   public void testInsertionSort() {
-      int[] array = { 5, 2, 9, 1, 5, 6 };
-      int[] expected = { 1, 2, 5, 5, 6, 9 };
+   public void testInsertionSortWithPositiveNumbers() {
+      int[] array = { 5, 2, 8, 1, 3 };
       InsertionAndSelectionSort.insertionSort(array);
-      assertArrayEquals(expected, array);
+      assertArrayEquals(new int[] { 1, 2, 3, 5, 8 }, array);
    }
 
    @Test
-   public void testInsertionSortEmptyArray() {
-      int[] array = {};
-      int[] expected = {};
-      InsertionAndSelectionSort.insertionSort(array);
-      assertArrayEquals(expected, array);
+   public void testSelectionSortWithPositiveNumbers() {
+      int[] array = { 5, 2, 8, 1, 3 };
+      InsertionAndSelectionSort.selectionSort(array);
+      assertArrayEquals(new int[] { 1, 2, 3, 5, 8 }, array);
    }
 
    @Test
-   public void testInsertionSortSingleElement() {
+   public void testInsertionSortWithNegativeNumbers() {
+      int[] array = { -3, -1, -4, -2, -5 };
+      InsertionAndSelectionSort.insertionSort(array);
+      assertArrayEquals(new int[] { -5, -4, -3, -2, -1 }, array);
+   }
+
+   @Test
+   public void testSelectionSortWithNegativeNumbers() {
+      int[] array = { -3, -1, -4, -2, -5 };
+      InsertionAndSelectionSort.selectionSort(array);
+      assertArrayEquals(new int[] { -5, -4, -3, -2, -1 }, array);
+   }
+
+   @Test
+   public void testInsertionSortWithMixedNumbers() {
+      int[] array = { 3, -1, 4, -2, 0 };
+      InsertionAndSelectionSort.insertionSort(array);
+      assertArrayEquals(new int[] { -2, -1, 0, 3, 4 }, array);
+   }
+
+   @Test
+   public void testSelectionSortWithMixedNumbers() {
+      int[] array = { 3, -1, 4, -2, 0 };
+      InsertionAndSelectionSort.selectionSort(array);
+      assertArrayEquals(new int[] { -2, -1, 0, 3, 4 }, array);
+   }
+
+   @Test
+   public void testInsertionSortWithDuplicates() {
+      int[] array = { 3, 1, 2, 1, 3 };
+      InsertionAndSelectionSort.insertionSort(array);
+      assertArrayEquals(new int[] { 1, 1, 2, 3, 3 }, array);
+   }
+
+   @Test
+   public void testSelectionSortWithDuplicates() {
+      int[] array = { 3, 1, 2, 1, 3 };
+      InsertionAndSelectionSort.selectionSort(array);
+      assertArrayEquals(new int[] { 1, 1, 2, 3, 3 }, array);
+   }
+
+   @Test
+   public void testInsertionSortWithSingleElement() {
       int[] array = { 1 };
-      int[] expected = { 1 };
       InsertionAndSelectionSort.insertionSort(array);
-      assertArrayEquals(expected, array);
+      assertArrayEquals(new int[] { 1 }, array);
    }
 
    @Test
-   public void testSelectionSort() {
-      int[] array = { 5, 2, 9, 1, 5, 6 };
-      int[] expected = { 1, 2, 5, 5, 6, 9 };
-      InsertionAndSelectionSort.selectionSort(array);
-      assertArrayEquals(expected, array);
-   }
-
-   @Test
-   public void testSelectionSortEmptyArray() {
-      int[] array = {};
-      int[] expected = {};
-      InsertionAndSelectionSort.selectionSort(array);
-      assertArrayEquals(expected, array);
-   }
-
-   @Test
-   public void testSelectionSortSingleElement() {
+   public void testSelectionSortWithSingleElement() {
       int[] array = { 1 };
-      int[] expected = { 1 };
       InsertionAndSelectionSort.selectionSort(array);
-      assertArrayEquals(expected, array);
+      assertArrayEquals(new int[] { 1 }, array);
    }
 
    @Test
-   public void testInsertionSortInstance() {
-      int[] array = { 5, 2, 9, 1, 5, 6 };
-      int[] expected = { 1, 2, 5, 5, 6, 9 };
-      InsertionAndSelectionSort sorter = new InsertionAndSelectionSort();
-      sorter.insertionSortInstance(array);
-      assertArrayEquals(expected, array);
+   public void testInsertionSortWithEmptyArray() {
+      int[] array = {};
+      InsertionAndSelectionSort.insertionSort(array);
+      assertArrayEquals(new int[] {}, array);
    }
 
    @Test
-   public void testSelectionSortInstance() {
-      int[] array = { 5, 2, 9, 1, 5, 6 };
-      int[] expected = { 1, 2, 5, 5, 6, 9 };
-      InsertionAndSelectionSort sorter = new InsertionAndSelectionSort();
-      sorter.selectionSortInstance(array);
-      assertArrayEquals(expected, array);
+   public void testSelectionSortWithEmptyArray() {
+      int[] array = {};
+      InsertionAndSelectionSort.selectionSort(array);
+      assertArrayEquals(new int[] {}, array);
+   }
+
+   @Test
+   public void testInsertionSortWithNullArray() {
+      assertThrows(IllegalArgumentException.class, () -> {
+         InsertionAndSelectionSort.insertionSort(null);
+      });
+   }
+
+   @Test
+   public void testSelectionSortWithNullArray() {
+      assertThrows(IllegalArgumentException.class, () -> {
+         InsertionAndSelectionSort.selectionSort(null);
+      });
    }
 }
